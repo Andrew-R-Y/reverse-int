c59f135d2f0b209f8b22c6d31cf78e99b3b573c3
Andrew-R-Y <rakitinandrei@gmail.com>
2023-01-15 13:58:09 +0300

    feat: update index.js - function reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const text = Math.abs(n).toString()
+  let result = '';
+  let i = 0;
+  while (i < text.length) {
+    result = text[i] + result
+    i++
+  }
+  return Number(result)
 }
